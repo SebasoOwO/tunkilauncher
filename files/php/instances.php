3568c5e5945e52c94481627aa23477aaad67cbb3
SebasoOwO <sebasgallegojimenez1@gmail.com>
2026-03-08 15:38:54 -0500

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -24,4 +24,21 @@ $instance['AG_GAMES'] = array_merge($instance['AG_GAMES'], array(
         "port" => 25565
     )
 ));
+
+$instance['hola'] = array_merge($instance['hola'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.20.1",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => false,
+    "ignored" => array(),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "PokeMoonX",
+        "ip" => "178.32.106.234",
+        "port" => 25599
+    )
+));
 ?>
